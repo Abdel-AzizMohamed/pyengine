f10d05f95b9bb72996ef0df59afb963441f8c87c
vagrant <vagrant@linux-vag20>
2023-09-26 15:32:11 +0000

    Add project base

---
+++ engine.py
@@ -0,0 +1,17 @@
+"""Define Engine file that contains the main logic"""
+import pygame
+
+
+class PyEngine():
+    """the main class that that works as a connector for all packges"""
+    def __init__(self):
+        """Init Engine Object"""
+
+    def mainloop(self):
+        """Game Mainloop"""
+
+    def load_assets(self):
+        """Load game assets that includes (audio, images, ui data, ...)"""
+
+    def trigger_events(self):
+        """trigger all game events"""
