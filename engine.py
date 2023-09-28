7af45091bd9e263eae6efa5be3a4ee7f0e9598ae
Zi-Max <katakuricharlottesama12@gmail.com>
2023-09-28 05:45:21 +0000

    Add window object to engine

--- engine.py
+++ engine.py
@@ -1,11 +1,17 @@
 """Define Engine file that contains the main logic"""
+###### Python Packges ######
+import sys
 import pygame
+
+###### My Packges ######
+from window import Window
 
 
 class PyEngine():
     """the main class that that works as a connector for all packges"""
     def __init__(self):
         """Init Engine Object"""
+        self.window = Window()
 
     def mainloop(self):
         """Game Mainloop"""
