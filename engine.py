1a43a73af36a41ec6e6bf4dcb11f2c6df32e0da2
Zi-Max <katakuricharlottesama12@gmail.com>
2023-10-05 13:45:22 +0000

    Add game exit event for testing

--- engine.py
+++ engine.py
@@ -15,6 +15,15 @@ class PyEngine():
 
     def mainloop(self):
         """Game Mainloop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
 
     def load_assets(self):
         """Load game assets that includes (audio, images, ui data, ...)"""
