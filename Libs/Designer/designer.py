a0b2968e24fab004001ebb7829f2d4e8a9c88ee3
Zi-Max <katakuricharlottesama12@gmail.com>
2023-10-31 18:52:45 +0200

    Clean designer file
    nothing had change only some cleaing and error handling

--- Libs/Designer/designer.py
+++ Libs/Designer/designer.py
@@ -1,23 +1,29 @@
-"""Define the main designer file"""
+"""Define the designer packge start point"""
+###### Python Packges ######
 import pygame
+
+###### My Packges ######
 from window import win_obj
 from Libs.Designer.py_elements import PyRect
 
 
-classes = {"PyRect": PyRect}
-
-
 class Designer:
-    """Define the main grahpic helper"""
+    """
+    Define the main grahpic helper
+
+    Attributes:
+            elements_classes: Contains all the elements classes
+    """
+    elements_classes = {"PyRect": PyRect}
 
     def __init__(self):
         """
         init a new Designer object
 
         Attributes:
-            elements: dict of all game elements
+            game_elements: dict of all game elements
         """
-        self.elements = {}
+        self.game_elements = {}
 
     def create_element(self, ele_class, ele_attributes):
         """
@@ -26,30 +32,44 @@ class Designer:
         Arguments:
             ele_class: element class name
             ele_attributes: element attributes (text, color, ...)
-            ele_group: element group
         """
-        element = classes[ele_class](ele_attributes)
+        if type(ele_attributes) != dict:
+            raise TypeError("ele_attributes should be a dictionary")
+
         ele_group = ele_attributes.get("group")
+        ele_name = ele_attributes.get("name")
 
-        if not self.elements.get(ele_group):
-            self.elements[ele_group] = {}
+        if not ele_name or not ele_group:
+            raise ValueError("provide Name & Group for the given element")
+        elif self.get_element(ele_name):
+            raise ValueError("The given name is already used before")
 
-        self.elements.get(ele_group)[element.name] = element
+        element = self.elements_classes[ele_class](ele_attributes)
+
+        if not self.game_elements.get(ele_group):
+            self.game_elements[ele_group] = {}
+
+        self.game_elements.get(ele_group)[ele_name] = element
 
     def read_ui(self, file_path):
         """read given ui file"""
 
     def get_element(self, name):
-        """gets element by its name"""
-        for group in self.elements.values():
-            for key, item in group.items():
-                if key == name:
-                    return item
+        """
+        gets element by its name
+
+        Arguments:
+            name: name to search for
+        """
+        elements = [(key, item) for group in self.game_elements.values() for (key, item) in group.items()]
+        for key, item in elements:
+            if key == name:
+                return item
         return None
 
     def draw_group(self):
         """draw elements on screen"""
-        for group in self.elements.values():
-            for item in group.values():
-                if item.type == "rect":
-                    pygame.draw.rect(win_obj.screen, "#FFFFFF", item.rect)
+        elements = [item for group in self.game_elements.values() for item in group.values()]
+        for item in elements:
+            if item.type == "rect":
+                pygame.draw.rect(win_obj.screen, "#FFFFFF", item.rect)
